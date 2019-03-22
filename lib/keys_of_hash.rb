a6a915c464bb966f3a4d92d5fd0a6018848c1048
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 20:02:44 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
   def keys_of(*arguments)
-    
+    output = []
+    Hash.each do |key, value|
+      if value == arguments
+        output.push(key.to_s)
+      end
+    end
+    return output
   end
 end
